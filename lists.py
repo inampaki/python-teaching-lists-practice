1c849f3154e2f21efcc70a67135f64683f2cf007
NomanIslam <noman.islam@gmail.com>
2019-05-05 05:02:30 -0700

    Practicing List, Coposite Conditions

--- lists.py
+++ lists.py
@@ -2,6 +2,7 @@ day_temperatures = [28, 28.9, 28, 29, 29, 30, 31, 32, 34, 36, 40, 32, 30]
 day_temperatures.append(49)
 day_temperatures.insert(4, 31)
 del day_temperatures[5]
+del day_temperatures[6]
 print(day_temperatures[13])
 print(day_temperatures[13])
 print(day_temperatures[13])
